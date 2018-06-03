df52782b6a3ac2204ecc95866a9adacee31e52eb
Matthew Russell <mrussell@neptec.com>
2018-06-03 14:08:46 -0400

    Added some system dependencies, and upgraded the script in general

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os, platform
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
 from conans.errors import ConanException
@@ -9,19 +12,19 @@ class FfiConan(ConanFile):
     this builds the static and shared ffi libs.
     """
 
-    name = 'ffi'
-    version = '3.2.1'
-    license = 'https://github.com/libffi/libffi/blob/master/LICENSE'
-    url = 'https://raw.githubusercontent.com/kheaactua/conan-ffi/v3.2.1/conanfile.py'
+    name        = 'ffi'
+    version     = '3.2.1'
+    license     = 'https://github.com/libffi/libffi/blob/master/LICENSE'
+    url         = 'https://raw.githubusercontent.com/kheaactua/conan-ffi/v3.2.1/conanfile.py'
     description = 'Compilers for high level languages generate code that follow certain conventions.'
-    settings = 'os', 'compiler', 'build_type', 'arch'
-    requires = 'helpers/[>=0.2.0]@ntc/stable',
+    settings    = 'os', 'compiler', 'build_type', 'arch'
+    requires    = 'helpers/[>= 0.3.0]@ntc/stable',
 
     def system_requirements(self):
 
         pack_names = None
         if tools.os_info.linux_distro == 'ubuntu':
-            pack_names = ['texinfo']
+            pack_names = ['texinfo', 'autoconf', 'libtool']
 
             if self.settings.arch == 'x86':
                 full_pack_names = []
@@ -58,17 +61,11 @@ class FfiConan(ConanFile):
         self.env_info.MANPATH.append(os.path.join(self.package_folder, 'share', 'man'))
 
         # Populate the pkg-config environment variables
-        import site; site.addsitedir(self.deps_cpp_info['helpers'].rootpath) # Compensate for #2644
-        from platform_helpers import adjustPath, appendPkgConfigPath
+        with tools.pythonpath(self):
+            from platform_helpers import adjustPath, appendPkgConfigPath
 
-        self.env_info.PKG_CONFIG_LIBFFI_PREFIX = adjustPath(self.package_folder)
-        appendPkgConfigPath(adjustPath(os.path.join(self.package_folder, 'lib', 'pkgconfig')), self.env_info)
-
-        # Populate the pkg-config environment variables
-        import site; site.addsitedir(self.deps_cpp_info['helpers'].rootpath) # Compensate for #2644
-        from platform_helpers import adjustPath, appendPkgConfigPath
-        self.env_info.PKG_CONFIG_FLANN_PREFIX = adjustPath(self.package_folder)
-        appendPkgConfigPath(adjustPath(os.path.join(self.package_folder, 'lib', 'pkgconfig')), self.env_info)
+            self.env_info.PKG_CONFIG_LIBFFI_PREFIX = adjustPath(self.package_folder)
+            appendPkgConfigPath(adjustPath(os.path.join(self.package_folder, 'lib', 'pkgconfig')), self.env_info)
 
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
